1ce0d8b91a0f4498375d582791be8ce469a2f3ff
Sergey Edalov <edalov94@gmail.com>
2024-03-18 12:07:19 +0300

    Homework_1

--- Homework_1/obj/Debug/net8.0/Homework_1.AssemblyInfo.cs
+++ Homework_1/obj/Debug/net8.0/Homework_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a1618c8446e12d4418827707cf2f454a43cb22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
